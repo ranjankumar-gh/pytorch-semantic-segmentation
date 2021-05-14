8abdc06e942044b12d5e73de0b743f7ab1682972
ranjankumar-gh <mail4ranjan@gmail.com>
2021-05-14 11:50:38 +0530

    Our first commit!

--- models/fcn8s.py
+++ models/fcn8s.py
@@ -64,6 +64,18 @@ class FCN8s(nn.Module):
         self.upscore2.weight.data.copy_(get_upsampling_weight(num_classes, num_classes, 4))
         self.upscore_pool4.weight.data.copy_(get_upsampling_weight(num_classes, num_classes, 4))
         self.upscore8.weight.data.copy_(get_upsampling_weight(num_classes, num_classes, 16))
+        
+    def get_upsampling_weight(in_channels, out_channels, kernel_size):
+        factor = (kernel_size + 1) // 2
+        if kernel_size % 2 == 1:
+            center = factor - 1
+        else:
+            center = factor - 0.5
+        og = np.ogrid[:kernel_size, :kernel_size]
+        filt = (1 - abs(og[0] - center) / factor) * (1 - abs(og[1] - center) / factor)
+        weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size), dtype=np.float64)
+        weight[list(range(in_channels)), list(range(out_channels)), :, :] = filt
+        return torch.from_numpy(weight).float()
 
     def forward(self, x):
         x_size = x.size()
