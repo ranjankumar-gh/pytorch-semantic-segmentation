e0a2d4c0c2d75a5e1ba0e0e2cae24190c37fd24c
ranjankumar-gh <mail4ranjan@gmail.com>
2021-05-14 12:23:48 +0530

    Our first commit!

--- models/fcn8s.py
+++ models/fcn8s.py
@@ -4,6 +4,7 @@ from torchvision import models
 
 #from ..utils import get_upsampling_weight
 #from .config import vgg16_path, vgg16_caffe_path
+from misc import get_upsampling_weight
 from config import vgg16_path, vgg16_caffe_path
 
 
